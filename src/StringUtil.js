a08a86556f3907a1b979e126e5d3956267ce2aa6
lansechahu <lansechahu@126.com>
2020-09-03 15:54:05 +0800

    update

--- src/StringUtil.js
+++ src/StringUtil.js
@@ -102,7 +102,7 @@ export default class StringUtil {
 			range = Math.round(Math.random() * (max - min)) + min;
 		}
 		for(var i = 0; i < range; i++) {
-			pos = Math.round(Math.random() * (arr.length - 1));
+			var pos = Math.round(Math.random() * (arr.length - 1));
 			str += arr[pos];
 		}
 		return str;
